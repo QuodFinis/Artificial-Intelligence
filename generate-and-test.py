6e03950e03937287c1d63648b588e55d28e8c6ef
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2023-10-25 18:12:38 -0400

    Create generate-and-test.py

---
+++ generate-and-test.py
@@ -0,0 +1,11 @@
+"""
+Suppose you must detennine whether a given number between 3 and 100.
+inclusive. is a prime. Recall that an integer N 2 is prime if its only factors
+are I and itself. So 17 and 23 are prime whereas 33 is not. because it is the
+product of 3 and II. Assume that you must solve this problem without benefit
+of a computer or pocket calculator. Your first attempt at a solution, using the
+generate-and-test approach. might look like the following pseudocode:
+"""
+
+def is-prime(n):
+  if
